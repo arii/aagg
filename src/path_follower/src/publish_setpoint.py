2275f2dafd8888ebfa9ccc522255942bc2764fcc
Your Name <you@example.com>
2018-06-14 11:27:25 -0400

    updated publish setpoint to be path generator

--- src/path_follower/src/publish_setpoint.py
+++ src/path_follower/src/publish_setpoint.py
@@ -1,32 +1,74 @@
 #!/usr/bin/env python
 
-from geometry_msgs.msg import Pose, PoseStamped, Point, Quaternion
+from geometry_msgs.msg import Pose, PoseStamped, Point, \
+                              Quaternion, PointStamped
+from nav_msgs.msg import Path
+
 from std_msgs.msg import Header
-
+import tf
 import rospy
-rospy.init_node("simple_pose_publisher")
-arm = 'r'
-root_frame = 'base_link'
-tool_frame = '%s_gripper_tool_frame' % arm
-pos = (.97, -0.18, 0.99)
-quat = (0, 0, 0, 1)
+pos = (.9, -0.18, 0.23)
+quat = (1, 0, 0, 0)
 
-pub = rospy.Publisher("%s_cart/command_pose" % arm,\
+class PathGenerator:
+    def __init__(self, arm, root_frame, tool_frame, tf_listener=None):
+        self.root_frame = root_frame
+        self.tool_frame = tool_frame
+        self.arm = arm
+        self.pub = rospy.Publisher("%s_cart/command_pose" % arm,\
                         PoseStamped, queue_size=1)
+        self.sub = rospy.Subscriber("/clicked_point", PointStamped, \
+                        self.updateOrigin)
+        self.origin_updated = False
+        self.origin = Point(0.9, -0.18, 0.23) #XXX todo : make this current pose
 
+        if tf_listener is None:
+            self.tf_listener = tf.TransformListener()
+        else:
+            self.tf_listener = tf_listener
 
-def stamp_pose((pos,quat), root_frame):
-    ps = PoseStamped( 
-            Header(0,rospy.Time(0),root_frame),\
+    def updateOrigin(self, msg):
+        if msg.header.frame_id != self.root_frame:
+            rospy.warn("need to convert to root frame")
+            raise NotImplementedError
+        else:
+            self.origin = msg.point
+            rospy.loginfo("updated origin to (%.2f %.2f %.2f)" % \
+                    (self.origin.x, self.origin.y, self.origin.z)
+                    )
+        self.circle(.5)
+
+    def stamp_pose(self, (pos,quat)):
+        ps = PoseStamped( 
+            Header(0,rospy.Time(0),self.root_frame),\
             Pose(Point(*pos),\
             Quaternion(*quat)))
-    return ps
+        return ps
 
+
+    def circle(self, r):
+        """ generator points around a circle with radius r"""
+        rospy.loginfo("generating a circle path")
+
+        #XXX TODO publish a nav_msgs path
+
+
+if __name__=="__main__":
+    rospy.init_node("path_generator")
+    arm = 'r'
+    root_frame = 'torso_lift_link'
+    tool_frame = '%s_gripper_tool_frame' % arm
+    pos = (.9, -0.18, 0.23)
+
+    pg = PathGenerator(arm, root_frame, tool_frame)
+    rospy.spin()
+
+"""
+rospy.sleep(1)
 cmd = stamp_pose( (pos,quat), root_frame )
 pub.publish(cmd)
 print cmd
 rospy.spin()
-"""
 def command_delta(x,y,z):
     pos, quat = get_pose()
     pos[0] += x
